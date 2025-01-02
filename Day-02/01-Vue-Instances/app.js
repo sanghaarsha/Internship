1dce1e096b84bd8ca4a93ea04e5d5677c2071604
sanghaarsha <sanghaarsha@gmail.com>
2025-01-02 11:02:17 +0545

    creating and interacting with a vue instance

---
+++ Day-02/01-Vue-Instances/app.js
@@ -0,0 +1,15 @@
+// creating a vue app
+const app = Vue.createApp({
+    // options available here are: data, methods, etc.
+    // 1. data: this is where we define the data that we want to use in our app
+    data: function () {
+        return {
+            learningGoals: "Learn Vue.js from scratch and master it!",
+        };
+    },
+    // 2. methods: this is where we define the methods that we want to use in our app
+    methods: undefined,
+});
+
+// This helps to control the div with the id my_goals
+app.mount("#my_goals");
