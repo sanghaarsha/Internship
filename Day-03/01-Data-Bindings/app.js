e2280b5ce036906cdcefee0d53540155b85f939e
sanghaarsha <sanghaarsha@gmail.com>
2025-01-03 21:06:39 +0545

    Two way binding

---
+++ Day-03/01-Data-Bindings/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            currentCount: 0,
+            username: "",
+        };
+    },
+
+    methods: {
+        increaseCount(num = 1) {
+            this.currentCount = this.currentCount + num;
+        },
+        decreaseCount(num = 1) {
+            if (this.currentCount > 0 && this.currentCount - num >= 0) {
+                this.currentCount = this.currentCount - num;
+            }
+        },
+        setName(e) {
+            this.username = e.target.value;
+        },
+        resetInput() {
+            this.username = "";
+        },
+    },
+});
+
+app.mount("#app");
